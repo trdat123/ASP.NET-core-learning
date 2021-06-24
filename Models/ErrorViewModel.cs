1635807a3268187250a6b5350c5d7babc044e286
Nguyen Dat <nkokviper123@gmail.com>
2021-06-24 22:16:31 +0700

    First commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Team_1_E_commerce.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
